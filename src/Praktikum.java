1843811630787fc74e15f94fec1a9e4940941aca
Evgenij Novikov <novikov.ev84@yandex.ru>
2022-07-07 19:49:04 +0300

    Разделяем траты по категориям

--- src/Praktikum.java
+++ src/Praktikum.java
@@ -1,63 +1,60 @@
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Praktikum {
     public static void main(String[] args) {
 
-        HashMap<String, Integer> musicGroups = new HashMap<>();
-        musicGroups.put("The Beatles", 1960);
-        musicGroups.put("Rammstein", 1994);
-        musicGroups.put("Queen", 1970);
-        musicGroups.put("Led Zeppelin", 1968);
-        musicGroups.put("Black Sabbath", 1968);
-        musicGroups.put("AC/DC", 1973);
-        musicGroups.put("The Rolling Stones", 1962);
-        musicGroups.put("The Who", 1964);
-        musicGroups.put("Nightwish", 1996);
-        musicGroups.put("Nirvana", 1987);
-        musicGroups.put("Guns N’ Roses", 1985);
-        musicGroups.put("Scorpions", 1965);
-        musicGroups.put("Kiss", 1973);
-        musicGroups.put("The Doors", 1965);
-
-        System.out.println("Легендарные группы, год основания которых вы можете узнать:");
-        for(String group : musicGroups.keySet()){
-            System.out.println(group);
-        }
-
-
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Сколько денег у вас осталось до зарплаты?");
+        double moneyBeforeSalary = scanner.nextDouble();
+        System.out.println("Сколько дней до зарплаты?");
+        int daysBeforeSalary = scanner.nextInt();
+
+        Converter converter = new Converter(78.5, 88.7, 0.75);
+        DinnerAdvisor dinnerAdvisor = new DinnerAdvisor();
+        ExpensesManager expensesManager = new ExpensesManager();
+
         while (true) {
-            System.out.println("Выберите опцию");
-            System.out.println("1 - Узнать год основания группы");
-            System.out.println("2 - Добавить любимую группу");
-            System.out.println("3 - Выйти из приложения");
-            int command = Integer.parseInt(scanner.nextLine());
+            printMenu();
+            int command = scanner.nextInt();
+
             if (command == 1) {
-                System.out.println("Введите название группы");
-                String keyGroup = scanner.nextLine();
-                Integer year = musicGroups.get(keyGroup);
-                System.out.println("Год основания " + keyGroup);
-                System.out.println(year);
+                System.out.println("Ваши сбережения: " + moneyBeforeSalary + " RUB");
+                System.out.println("В какую валюту хотите конвертировать? Доступные варианты: 1 - USD, 2 - EUR, 3 - JPY.");
+                int currency = scanner.nextInt();
+                converter.convert(moneyBeforeSalary, currency);
             } else if (command == 2) {
-
-                System.out.println("Добавьте свою любимую группу");
-                System.out.println("Введите её название");
-                String nameGroup = String.valueOf(scanner.nextLine());
-                if (!musicGroups.containsKey(nameGroup)){
-                    System.out.println("Введите год основания");
-                    int bornYear = Integer.parseInt(String.valueOf(scanner.nextLine()));
-                    musicGroups.put(nameGroup, bornYear);
-                    System.out.println("Информация о группе " + nameGroup + " добавлена.");
-                } else {
-                    System.out.println("Группа уже есть в списке");
-                }
-
+                dinnerAdvisor.getAdvice(moneyBeforeSalary, daysBeforeSalary);
             } else if (command == 3) {
-              break;
+                System.out.println("Введите размер траты:");
+                double expense = scanner.nextDouble();
+                System.out.println("К какой категории относится трата?");
+                String category = scanner.next();
+                moneyBeforeSalary = expensesManager.saveExpense(moneyBeforeSalary, expense, category);
+            } else if (command == 4) {
+               expensesManager.printAllExpensesByCategories();
+            } else if (command == 5) {
+                System.out.println("В какой категории искать?");
+                String category = scanner.next();
+                System.out.println("Самая большая трата в категории " + category + " составила " + expensesManager.findMaxExpenseInCategory(category) + " руб.");
+            } else if (command == 6) {
+                expensesManager.removeAllExpenses();
+            } else if (command == 0) {
+                System.out.println("Выход");
+                break;
             } else {
                 System.out.println("Извините, такой команды пока нет.");
             }
         }
+    }
+
+    public static void printMenu() {
+        System.out.println("Что вы хотите сделать? ");
+        System.out.println("1 - Конвертировать валюту");
+        System.out.println("2 - Получить совет");
+        System.out.println("3 - Ввести трату");
+        System.out.println("4 - Показать траты по категориям");
+        System.out.println("5 - Показать самую большую трату в выбранной категории");
+        System.out.println("6 - Очистить таблицу трат");
+        System.out.println("0 - Выход");
     }
 }
